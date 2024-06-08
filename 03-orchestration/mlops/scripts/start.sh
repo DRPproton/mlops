db2d4ebd58e89a9458679501bb597e10ec2f2525
dashel ruiz <dashel.ruiz@protonmail.com>
2024-06-08 11:45:35 -0600

    Global products week3 working

--- 03-orchestration/mlops/scripts/start.sh
+++ 03-orchestration/mlops/scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROJECT_NAME=mlops \
-  MAGE_CODE_PATH=/home/src \
-  SMTP_EMAIL=$SMTP_EMAIL \
-  SMTP_PASSWORD=$SMTP_PASSWORD \
-  docker compose up
+export PROJECT_NAME=mlops
+export MAGE_CODE_PATH=home/src
+export SMTP_EMAIL=$SMTP_EMAIL
+export SMTP_PASSWORD=$SMTP_PASSWORD
+docker-compose up
